2eb3dee9467b0bdbdd796b5692ef5698c53147d4
Tim dinia <diniahatim@gmail.com>
2019-10-17 19:31:58 +0200

    feat(lang): add fr language internationalization

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,23 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import fr from 'vuetify/es5/locale/fr';
 
 Vue.use(Vuetify);
 
+Vue.component('../components/Calendar.vue', {
+  methods: {
+    changeLocale() {
+      this.$vuetify.lang.current = 'fr';
+    }
+  }
+});
+
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi',
+    iconfont: 'mdi'
   },
+  lang: {
+    locales: { fr },
+    current: 'fr'
+  }
 });
